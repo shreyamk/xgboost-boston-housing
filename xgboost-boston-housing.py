9145119f1def1c00be73072d1977273893486843
Shreya Kuruvilla <shreyamk94@gmail.com>
2020-08-07 23:34:54 -0700

    First try - xgboost

--- xgboost-boston-housing.py
+++ xgboost-boston-housing.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_boston
 import matplotlib.pyplot as plt
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
@@ -53,3 +54,12 @@ print(X.head())
 # Convert to dmatrix for enhanced performance
 data_dmatrix = xgb.DMatrix(data=X,label=y)
 
+# Split into training and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+# First attempt at xgboost
+xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1, max_depth = 5, alpha = 10, n_estimators = 10)
+xg_reg.fit(X_train,y_train)
+preds = xg_reg.predict(X_test)
+rmse = np.sqrt(mean_squared_error(y_test, preds))
+print("RMSE: %f" % (rmse))
